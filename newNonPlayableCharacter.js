725bd3b3eadcea1ba7c78547f136dfad3d67bfcb
michaelangelesz <mmweisphal@outlook.com>
2023-05-20 12:56:55 -0400

    fixed async directions, wrote complicated path for npc

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -27,28 +27,28 @@ function newNonPlayableCharacter(x, y) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
         await sleep(time)
-        stop();
+        stop()
     }
 
-    async function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
         await sleep(time)
-        stop();
+        stop()
     }
 
-    async function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
         await sleep(time)
-        stop();
+        stop()
     }
 
-    async function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
         await sleep(time)
-        stop();
+        stop()
     }
 
     function stop() {
