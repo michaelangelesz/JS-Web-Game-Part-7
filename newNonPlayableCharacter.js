9563d1bdbaa1d2baec7e0b35e3a39e2b440c4b6e
michaelangelesz <mmweisphal@outlook.com>
2023-05-20 12:14:53 -0400

    made all the rest of the directions asynce functions

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -30,22 +30,25 @@ function newNonPlayableCharacter(x, y) {
         stop();
     }
 
-    function walkNorth() {
+    async function walkNorth() {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
-        
+        await sleep(time)
+        stop();
     }
 
-    function walkWest() {
+    async function walkWest() {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
-        
+        await sleep(time)
+        stop();
     }
 
-    function walkSouth() {
+    async function walkSouth() {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
-        
+        await sleep(time)
+        stop();
     }
 
     function stop() {
