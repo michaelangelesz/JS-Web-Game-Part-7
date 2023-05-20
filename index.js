eaeedd04cc2241854556dff05679b2ce8896be1e
michaelangelesz <mmweisphal@outlook.com>
2023-05-20 13:09:57 -0400

    bonus:  added while loop to async function to make character keep running in circles

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
 async function moveNPC(){
+    while (true){
     await npc.walkNorth(1400)
     await npc.walkEast(1200)
     await npc.walkSouth(300)
@@ -13,6 +14,7 @@ async function moveNPC(){
     await npc.walkSouth(1500)
     await npc.walkWest(2700)
     await npc.walkNorth(400)
+}
 }
 moveNPC()
 
